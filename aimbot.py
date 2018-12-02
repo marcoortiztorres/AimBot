8bf34abc6407201b7bc45fccdcfc8c2e17dead79
marcoortiztorres <maor9361@colorado.edu>
2018-12-02 16:03:30 -0700

    prints the x and y of of the center of a dark object on a white background

--- aimbot.py
+++ aimbot.py
@@ -49,10 +49,11 @@ while True:
 
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-	thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+	thresh = cv2.threshold(blurred, 60, 225, cv2.THRESH_BINARY_INV)[1]
 
 	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
 	cv2.CHAIN_APPROX_SIMPLE)
+	# print(cnts)
 	cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 
 	# loop over the contours
@@ -67,14 +68,10 @@ while True:
 		# draw the contour and center of the shape on the image
 		cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
 		cv2.circle(image, (cX, cY), 7, (255, 255, 255), -1)
-		cv2.putText(image, "center", (cX - 20, cY - 20),
+		cv2.putText(image, str(cX)+","+str(cY), (cX - 20, cY - 20),
 			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
-		# # show the image
-		# cv2.imshow("Image", image)
-		# cv2.waitKey(0)
 
-	#
 	# # grab the frame dimensions and convert it to a blob
 	# (h, w) = frame.shape[:2]
 	# blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
@@ -111,7 +108,7 @@ while True:
 	# 			cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
 
 	# show the output frame
-	cv2.imshow("Frame", frame)
+	cv2.imshow("Frame", image)
 	key = cv2.waitKey(1) & 0xFF
 
 	# if the `q` key was pressed, break from the loop
