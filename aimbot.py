8ece1aea18bf6db7526982e0227d647493d50936
Lucas Lyon <lucas.a.lyon@gmail.com>
2018-12-11 19:27:45 -0700

    removed redundant code

--- aimbot.py
+++ aimbot.py
@@ -5,57 +5,11 @@
 from imutils.video import VideoStream
 from imutils.video import FPS
 from collections import OrderedDict
-from scipy.spatial import distance as dist
 import numpy as np
 import argparse
 import imutils
 import time
 import cv2
-
-class ColorLabeler:
-	def __init__(self):
-		#Create color dictionary
-		colors = OrderedDict({"green": (0,255,0), "red": (255,0,0), "blue": (0,0,255)})
-
-		# allocate memory for the L*a*b* image, then initialize
-		# the color names list
-		self.lab = np.zeros((len(colors), 1, 3), dtype="uint8")
-		self.colorNames = []
-
-		# loop over the colors dictionary
-		for (i, (name, rgb)) in enumerate(colors.items()):
-			# update the L*a*b* array and the color names list
-			self.lab[i] = rgb
-			self.colorNames.append(name)
-
-		# convert the L*a*b* array from the RGB color space
-		# to L*a*b*
-		self.lab = cv2.cvtColor(self.lab, cv2.COLOR_RGB2LAB)
-
-	def label(self, image, c):
-		# construct a mask for the contour, then compute the
-		# average L*a*b* value for the masked region
-		mask = np.zeros(image.shape[:2], dtype="uint8")
-		cv2.drawContours(mask, [c], -1, 255, -1)
-		mask = cv2.erode(mask, None, iterations=2)
-		mean = cv2.mean(image, mask=mask)[:3]
-
-		# initialize the minimum distance found thus far
-		minDist = (np.inf, None)
-
-		# loop over the known L*a*b* color values
-		for (i, row) in enumerate(self.lab):
-			# compute the distance between the current L*a*b*
-			# color value and the mean of the image
-			d = dist.euclidean(row[0], mean)
-
-			# if the distance is smaller than the current distance,
-			# then update the bookkeeping variable
-			if d < minDist[0]:
-				minDist = (d, i)
-
-		# return the name of the color with the smallest distance
-		return self.colorNames[minDist[1]]
 
 
 # construct the argument parse and parse the arguments
