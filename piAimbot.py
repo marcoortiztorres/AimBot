1a2ec7a1e2edc54700006d4fae2d35a5e1fea378
Lucas Lyon <lucas.a.lyon@gmail.com>
2018-12-16 13:27:55 -0700

    Final Code for pi

--- piAimbot.py
+++ piAimbot.py
@@ -1,3 +1,4 @@
+
 # USAGE
 # python real_time_object_detection.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
 
@@ -15,7 +16,7 @@ from time import sleep
 
 #setting up Bluetooth connection
 bluetoothSerial = serial.Serial("/dev/rfcomm1", baudrate=9600)
-
+#bt2 = serial.Serial("/dev/rfcomm0", baudrate=9600)
 
 # initialize the video stream, allow the cammera sensor to warmup,
 # and initialize the FPS counter
@@ -23,16 +24,19 @@ print("[INFO] starting video stream...")
 #vs = VideoStream(src=0).start()
 vs = VideoStream(usePiCamera=True).start()
 
-time.sleep(1.0)
+time.sleep(0.5)
 fps = FPS().start()
 
 # while there is live video loop thorugh each frame
 while True:
 	frame = vs.read()
-	frame = imutils.resize(frame, width=800)
+	frame = imutils.resize(frame, width=200)
 
 	commandX = ""
 	commandY = ""
+	commandLED = 'G'
+	xCentered = False
+	yCentered = False
 
 	# for copy pasta code
 	image = frame
@@ -40,21 +44,11 @@ while True:
 	blurred = cv2.GaussianBlur(image, (5, 5), 0)
 	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
-	# lower_green = np.array([40, 20, 20])
-	# upper_green = np.array([70,255,255])
-
 	lower_red_1 = np.array([0,70,50])
 	upper_red_1 = np.array([10,255,255])
 
 	lower_red_2 = np.array([170,70,50])
 	upper_red_2 = np.array([180,255,255])
-
-	# lower_blue = np.array([25, 50, 50])
-	# upper_blue = np.array([32,255,255])
-
-	# green_mask = cv2.inRange(hsv, lower_green, upper_green)
-	# _,green_contours,_ = cv2.findContours(green_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
 	red_mask1 = cv2.inRange(hsv, lower_red_1, upper_red_1)
 	red_mask_2 = cv2.inRange(hsv, lower_red_2, upper_red_2)
 
@@ -62,65 +56,58 @@ while True:
 
 	_, red_contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-	# blue_mask = cv2.inRange(hsv, lower_blue, upper_blue)
-	# _, blue_contours, _ = cv2.findContours(blue_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-	# cv2.drawContours(image, green_contours,-1,cvScalar(0, 255, 0),3)
-	# cv2.drawContours(image, red_contours, -1, cvScalar(255, 0, 0), 3)
-
-	# for contour in green_contours:
-	# 	area = cv2.contourArea(contour)
-	# 	if area > 1000:
-	# 		cv2.drawContours(image, contour, -1, (0, 255, 0), 3)
-			#print(area)
-
+	itemFound = False
 	for contour in red_contours:
 		area = cv2.contourArea(contour)
 
-		# compute center of contour
-		M = cv2.moments(contour)
-		if M["m00"] == 0.0:
-			M["m00"] = 0.001
-		cX = int(M["m10"] / M["m00"])
-		cY = int(M["m01"] / M["m00"])
-
 		if area > 1000:
-			cv2.drawContours(image, contour, -1, (0, 0, 255), 3)
-			cv2.circle(image, (cX, cY), 7, (255, 255, 255), -1)
-			cv2.putText(image, str(cX)+","+str(cY), (cX - 20, cY - 20),
-				cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+			M = cv2.moments(contour)
+			if M["m00"] == 0.0:
+				M["m00"] = 0.001
+			cX = int(M["m10"] / M["m00"])
+			cY = int(M["m01"] / M["m00"])
+			#cv2.drawContours(image, contour, -1, (0, 0, 255), 3)
+			#cv2.circle(image, (cX, cY), 7, (255, 255, 255), -1)
+			#cv2.putText(image, str(cX)+","+str(cY), (cX - 20, cY - 20),
+			#	cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+			#print("X: ", cX, " Y: ", cY)
 
-			if (cX < 390):
-				commandX = 'L'
-			elif (cX > 410):
-				commandX = 'R'
-			if (cY < 390):
-				commandY = 'D'
-			elif (cY > 410):
-				commandY = 'U'
+			if (cX < 80):
+				commandX = 'D' #'R'
+			elif (cX > 120):
+				commandX = 'U' #'L'
+			else: xCentered = True
+			if (cY < 80):
+				commandY = 'R' #'U'
+			elif (cY > 120):
+				commandY = 'L' #'D'
+			else:
+
+                            yCentered = True
+			itemFound = True
+
 			break
-
-	# for contour in blue_contours:
-	# 	area = cv2.contourArea(contour)
-	# 	if area > 1000:
-	# 		cv2.drawContours(image, contour, -1, (255, 0, 0), 3)
-			#print(area)
-
-	# all_masks = green_mask | red_mask | blue_mask
-	all_masks = red_mask
-
-	res = cv2.bitwise_and(frame, frame, mask=all_masks)
-	res = cv2.medianBlur(res, 5)
+	if not itemFound:
+		commandY = 'L'
+	if (xCentered and yCentered and itemFound):
+		commandLED = 'K'
+	#res = cv2.bitwise_and(frame, frame, mask=red_mask)
+	#res = cv2.medianBlur(res, 5)
 
 	# cv2.imshow('Original image', frame)
-	cv2.imshow('Color Detector', res)
-	# cv2.imshow('mask',all_masks)
+	#cv2.imshow('Color Detector', res)
+	#cv2.imshow('mask',red_mask)
 	if (commandX != ""):
 		bluetoothSerial.write(commandX.encode())
+		#bluetoothSerial.write(str("X").encode())
 	if (commandY != ""):
 		bluetoothSerial.write(commandY.encode())
+		#bluetoothSerial.write(str("X").encode())
+	time.sleep(0.1)
 	bluetoothSerial.write(str("X").encode())
-	print("\nCommandX: ", commandX, "\nCommand Y: ", commandY)
+	bluetoothSerial.write(commandLED.encode())
+	#print("\nCommandX: ", commandX, "\nCommand Y: ", commandY)
 	# Check if the user pressed ESC key
 	c = cv2.waitKey(1)
 	if c == 27:
